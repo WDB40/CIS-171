96a1d93ef4c9b6f7fc3e6dbcb145810963e3214a
WDB40 <wbrown1640@gmail.com>
2018-09-14 17:42:37 -0500

    Fixing some issues.

--- Week3/src/R2_13.java
+++ Week3/src/R2_13.java
@@ -9,22 +9,22 @@ public class R2_13 {
 
     }
 
-    static void dissectWord(String word){
+    private static void dissectWord(String word){
 
         char firstLetter = word.charAt(0);
         char lastLetter = word.charAt(word.length() - 1);
-        String middleString = word.substring(1, word.length()-2);
+        String middleString = word.substring(1, word.length()-1);
 
-        System.out.printf("The first letter: %c", firstLetter);
-        System.out.printf("The last letter: %c", lastLetter);
-        System.out.printf("The middle letters: %s", middleString);
+        System.out.printf("The first letter: %c\n", firstLetter);
+        System.out.printf("The last letter: %c\n", lastLetter);
+        System.out.printf("The middle letters: %s\n", middleString);
     }
 
-    static String getWord() {
+    private static String getWord() {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please enter the word to dissect:");
+        System.out.print("Please enter the word to dissect: ");
         return scanner.nextLine();
     }
 }
