1a4af4592adacbe04acd69815a57cff95ab8bfd1
WDB40 <wbrown1640@gmail.com>
2018-09-15 17:15:59 -0500

    Completing the HelloUser assignment.

---
+++ Week3/src/HelloUserBrown.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+
+public class HelloUserBrown {
+
+    public static void main(String[] args) {
+
+        String name = getName();
+        int age = getAge();
+        int ageNextYear = getAgeNextYear(age);
+        displayAgeNextYear(ageNextYear, name);
+    }
+
+    private static String getName(){
+
+        //Get the name from the user and then correct the format of it.
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+        name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+
+        return name;
+    }
+
+    private static int getAge(){
+
+        //Get the current age of the user
+        Integer age = null;
+
+        //Continue asking for a valid age until the user provides one
+        while(age==null){
+
+            try{
+                age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "A valid age is an integer. Please try again.");
+            }
+        }
+        return age;
+    }
+
+    private static int getAgeNextYear(int age) {
+
+        return ++age;
+    }
+
+    private static void displayAgeNextYear(int ageNextYear, String name) {
+
+        String message = String.format("%s, your age next year will be %d!", name, ageNextYear);
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
