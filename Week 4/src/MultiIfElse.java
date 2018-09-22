4ca20bd8435e548057cc8349682e55c42cdf7579
WDB40 <wbrown1640@gmail.com>
2018-09-22 15:36:20 -0500

    Doing the multi-if-else test.

---
+++ Week 4/src/MultiIfElse.java
@@ -0,0 +1,31 @@
+public class MultiIfElse {
+
+    public static void main(String[] args) {
+
+        System.out.println(numberOfStars(6));
+        System.out.println(numberOfStars(20));
+        System.out.println(numberOfStars(2));
+        System.out.println(numberOfStars(-1));
+        System.out.println(numberOfStars(7));
+
+    }
+
+    private static int numberOfStars(int value) {
+
+        int numberOfStars = 0;
+
+        if (value > 10) {
+            numberOfStars += 5;
+        } else if (value > 7) {
+            numberOfStars += 4;
+        } else if (value > 4) {
+            numberOfStars += 3;
+        } else if (value > 1) {
+            numberOfStars += 2;
+        }
+
+        numberOfStars++;
+
+        return numberOfStars;
+    }
+}
